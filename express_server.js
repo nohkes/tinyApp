9de9f4349aac823f1c7f4d2d7224de80763cfd9d
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-10-03 22:15:34 +0000

    merging

--- express_server.js
+++ express_server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
-
+var cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 function generateRandomString () {
@@ -13,28 +14,52 @@ return r;
 }
 
 var giveMeNum = generateRandomString();
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({extended: true}));
+
+
+// let templateVars = {
+//   username: req.cookies["username"],
+//   // ... any other vars
+// };
+
+
+
+
+
+
+app.post("/login", (req, res) => {
+	res.cookie("username", req.body.username)
+	res.redirect("/urls")
+})
+
+app.post("/logout", (req, res) => {
+	res.clearCookie("username", req.body.username)
+	res.redirect("/urls")
+})
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
-
-
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, 
+  username: req.cookies["username"]}
   res.render("urls_index", templateVars)	
 });
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  username: req.cookies["username"]
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
@@ -46,6 +71,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body); 
   urlDatabase[giveMeNum] = req.body.longURL;
+
   // let shortURL = generateRandomString() // debug statement to see POST parameters
   // urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls/" + giveMeNum);         // Respond with 'Ok' (we will replace this)
